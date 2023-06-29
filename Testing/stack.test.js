53a8d7ac8ba47f6b08536ecfdcf2c09cab977522
Monster <111894679+FilipFilchev@users.noreply.github.com>
2023-06-29 14:05:15 +0300

    tests added

---
+++ Testing/stack.test.js
@@ -0,0 +1,47 @@
+
+class Stack {
+constructor(){
+    this.top = -1;
+    this.items = {};
+}
+
+}
+
+describe('My Stack', () => {
+
+    it.todo('empty', () => {
+        const stack = new Stack();
+
+        expect(stack.top).toBe(-1);
+        expect(stack.items).toEqual({});
+    });
+
+    it.todo('can push to the top');
+
+})
+
+
+
+
+// cypress tool
+
+// [
+//     automative tests
+
+//     integration tests
+
+//     End to End testing
+
+//     unit tests
+
+// ]
+
+
+// -acceptance
+
+// --system
+
+// ---smoke tests
+
+// ----stress testing
+
---
+++ Testing/testing.py
@@ -0,0 +1,13 @@
+
+
+x = 11
+assert x > 0, "x should be positive"
+assert x % 2 == 0, "x should be even"
+
+system = False
+
+while system:
+    print("Happy, happy, happy")
+
+if system != True:
+    raise ValueError("Not Happy")
